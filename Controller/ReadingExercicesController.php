eeedc62012e50c55f80c76d8e06c0169431c3a5b
Videl Smith <videl@fslhome.org>
2014-04-11 23:22:56 +0200

    Adds and fixes
    Adds pictures for the homepage.
    Fix the base.html.twig/index.html.twig layout.

--- Controller/ReadingExercicesController.php
+++ Controller/ReadingExercicesController.php
@@ -23,6 +23,11 @@ class ReadingExercicesController extends Controller
 
     public function qa_documentsAction()
     {
+        $flash = $this->get('braincrafted_bootstrap.flash');
+        $flash->alert('This is an alert flash message.');
+        $flash->error('This is an error flash message.');
+        $flash->info('This is an info flash message.');
+        $flash->success('This is an success flash message.');
         return $this->render('TOEICTrainerBundle:ReadingExercices:index.html.twig');
     }
 }
