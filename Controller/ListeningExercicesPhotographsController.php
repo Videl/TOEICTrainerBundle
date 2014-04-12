6932edbc1540c736d49fe53aea6b940638bd6539
Clément SCHANEN <clement.schanen@telecomnancy.net>
2014-04-12 20:07:39 +0200

    Adds: Shuffle of the sound array
    Adds: Redirection with correction after the exercise

--- Controller/ListeningExercicesPhotographsController.php
+++ Controller/ListeningExercicesPhotographsController.php
@@ -3,6 +3,7 @@
 namespace TN\TOEICTrainerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class ListeningExercicesPhotographsController extends Controller
 {
@@ -13,12 +14,7 @@ class ListeningExercicesPhotographsController extends Controller
         $em = $this->getDoctrine()->getManager();
 
         $exercise = $em->getRepository('TOEICTrainerBundle:ListeningPhotographs')->find($id);
-        $soundsArray = $em->getRepository('TOEICTrainerBundle:AudioFile')->findAll();
-
-        /*foreach($soundsArray as $sound)
-        {
-          echo $exercise->getId();
-        }*/
+        $soundsArray = $em->getRepository('TOEICTrainerBundle:AudioFile')->findBy(array('type' => 'Photography'));
 
         if (!$exercise) {
             throw $this->createNotFoundException('Unable to find ListeningPhotographs exercise.');
@@ -26,7 +22,18 @@ class ListeningExercicesPhotographsController extends Controller
 
         return $this->render('TOEICTrainerBundle:ListeningExercices:train.photographs.html.twig', array(
             'exercise'      => $exercise, 
-            'sounds'        => $soundsArray,     
+            'sounds'        => $soundsArray,
+            'id'            => $id,    
             ));  
+    }
+
+    public function showAnswerAction($id, $result)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $exercise = $em->getRepository('TOEICTrainerBundle:ListeningPhotographs')->find($id);
+        return $this->render('TOEICTrainerBundle:ListeningExercices:train.photographs.answer.html.twig', array(
+            'exercise'      => $exercise,
+            'result'        => $result,
+            ));
     }
 }
